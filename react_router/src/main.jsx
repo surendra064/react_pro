1246a28374dbb65327dcda4f1a33d3dbb0fcdbe7
surendra064 <129845824+surendra064@users.noreply.github.com>
2024-09-16 06:30:19 +0000

    jhb

--- react_router/src/main.jsx
+++ react_router/src/main.jsx
@@ -2,10 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {RouterProvider} from 'react-router-dom'
+import {RouterProvider,createBrowserRouter,createRoutesFromElements,Route} from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import User from './components/User.jsx'
+import Github,{githubInfoLoader} from './components/Github.jsx'
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      <Route path='github' loader={githubInfoLoader} element={<Github/>}/>
 
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   ro
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
